0bf15d291925fe9c85f827d5b0b12c07e1fde880
johnpstratton <john.p.stratton6@gmail.com>
2020-04-05 22:07:38 -0700

    about-me updates for submission

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-'use strict.';
+// 'use strict.'
 // // Let's talk about me game
 // // Give user a set amount of time to read biography
 // // Intro will be three minutes of read time followed by quiz
@@ -12,14 +12,14 @@ var yourName = prompt('What is your name');
 alert('Thanks for visiting my page ' + yourName + ' please take the quiz by clicking the button below after reading my biography');
 
 
-// // Clicking the button will generate the function to ask questions about my bio
-// // document.getElementById('toFunction').onclick = doQuestions;
+// Clicking the button will generate the function to ask questions about my bio
+document.getElementById('toFunction').onclick = doQuestions;
 // // var oneSecond = 1000;
 // // var oneMinute = oneSecond * 60;
 // // var thirtySeconds = oneSecond * 30;
 // // .log (oneMinute);
-// // onclick (toFunction)= setTimeout (getReady, thirtySeconds);
-// // var getReady = alert ('Get Ready');
+// onclick (toFunction)= setTimeout (getReady, thirtySeconds);
+// var getReady = alert ('Get Ready');
 // //  (getReady + 'Get Ready');  
 
 
@@ -28,118 +28,117 @@ var questionRight = 0;
 var guessCount = 0;
 
 
-// // function doQuestions () {
+function doQuestions() {
 
-function Question1() {
-  const questionOneAnswer = ('yes');
-  var questionOneResponse = prompt('Is my favorite color blue').toLowerCase();
-  if (questionOneResponse === questionOneAnswer) {
-    alert('Great Job!');
-    questionRight++;
-  } else {
-    alert('Try Again');
-  }
-}
-
-function Question2() {
-  const questionTwoAnswer = ('no')
-  var questionTwoResponse = prompt('Did I grow up in Philadelphia?').toLowerCase();
-  if (questionTwoResponse === questionTwoAnswer) {
-    alert('You got it!');
-    questionRight++;
-  } else {
-    alert('So close!');
-  }
-}
-
-function Question3() {
-  const questionThreeAnswer = ('yes')
-  var questionThreeResponse = prompt('Did I play goalkeeper in soccer?').toLowerCase();
-  if (questionThreeResponse === questionThreeAnswer) {
-    alert('Thumbs up!');
-    questionRight++;
-  } else {
-    alert('Try Again');
-  }
-}
-
-function Question4() {
-  const questionFourAnswer = ('yes');
-  var questionFourResponse = prompt('Was I in the Army').toLowerCase();
-  if (questionFourResponse === questionFourAnswer) {
-    alert('Yay you did it!');
-    questionRight++;
-  } else {
-    alert('Come on stinkbrain');
-  }
-}
-
-function Question5() {
-  const questionFiveAnswer = ('yes')
-  var questionFiveResponse = prompt('Did I join the Army in 2013').toLowerCase();
-  if (questionFiveResponse === questionFiveAnswer) {
-    alert('Heck Yes');
-    questionRight++;
-  } else {
-    alert('Try again sucka');
-  }
-}
-
-
-// // const questionSixAnswer= 7;
-// // let questionSixResponse= prompt ('How many years did I serve in the Army?');
-// // if (questionSixResponse === questionSixAnswer) {
-// //   alert ('Yay!'); 
-// //   questionRight ++;
-// // } else if (questionSixResponse > questionSixAnswer) {
-// //   alert ('Try a lower number');
-// // } else (questionSixResponse < questionSixAnswer) 
-// //   alert('Try a higher number');
-// // guessCount ++;
-
-
-
-function Question6() {
-  do {
-    var questionSix = prompt('How many years did I serve in the Army? 1-20');
-    if (questionSix == 7) {
-      alert('Good Job')
-      questionRight++
-      break;
-    } else if (questionSix < 7) {
-      alert('That was too low')
-    } else if (questionSix > 7) {
-      alert('That was too high')
-    } guessCount++
-  } while (guessCount < 4);
-}
-
-
-function Question7() {
-  do {
-    var favMovies = ['harry potter', 'good will hunting', 'grandma\'s boy', 'drive', 'step brothers', 'forrest gump', 'any monty python', 'tropic thunder', 'saving private ryan', 'a beautiful mind'];
-    var favMoviesQuestion = prompt('What do you think my favorite movies are?');
-    if (favMovies[0] === favMoviesQuestion || favMovies[1] === favMoviesQuestion) {
-      alert('Good Job!')
-      questionRight++
-      break;
-    } else (!favMoviesQuestion); {
-      alert('Please try again')
+  function Question1() {
+    const questionOneAnswer = ('yes');
+    var questionOneResponse = prompt('Is my favorite color blue').toLowerCase();
+    if (questionOneResponse === questionOneAnswer) {
+      alert('Great Job!');
+      questionRight++;
+    } else {
+      alert('Try Again');
     }
-    guessCount++
-  } while (guessCount < 6);
-  alert('Here is a list of my favorite movies ' + favMovies);
+  }
+
+  function Question2() {
+    const questionTwoAnswer = ('no')
+    var questionTwoResponse = prompt('Did I grow up in Philadelphia?').toLowerCase();
+    if (questionTwoResponse === questionTwoAnswer) {
+      alert('You got it!');
+      questionRight++;
+    } else {
+      alert('So close!');
+    }
+  }
+
+  function Question3() {
+    const questionThreeAnswer = ('yes')
+    var questionThreeResponse = prompt('Did I play goalkeeper in soccer?').toLowerCase();
+    if (questionThreeResponse === questionThreeAnswer) {
+      alert('Thumbs up!');
+      questionRight++;
+    } else {
+      alert('Try Again');
+    }
+  }
+
+  function Question4() {
+    const questionFourAnswer = ('yes');
+    var questionFourResponse = prompt('Was I in the Army').toLowerCase();
+    if (questionFourResponse === questionFourAnswer) {
+      alert('Yay you did it!');
+      questionRight++;
+    } else {
+      alert('Come on stinkbrain');
+    }
+  }
+
+  function Question5() {
+    const questionFiveAnswer = ('yes')
+    var questionFiveResponse = prompt('Did I join the Army in 2013').toLowerCase();
+    if (questionFiveResponse === questionFiveAnswer) {
+      alert('Heck Yes');
+      questionRight++;
+    } else {
+      alert('Try again sucka');
+    }
+  }
+
+
+  // // const questionSixAnswer= 7;
+  // // let questionSixResponse= prompt ('How many years did I serve in the Army?');
+  // // if (questionSixResponse === questionSixAnswer) {
+  // //   alert ('Yay!'); 
+  // //   questionRight ++;
+  // // } else if (questionSixResponse > questionSixAnswer) {
+  // //   alert ('Try a lower number');
+  // // } else (questionSixResponse < questionSixAnswer) 
+  // //   alert('Try a higher number');
+  // // guessCount ++;
+
+
+
+  function Question6() {
+    do {
+      var questionSix = prompt('How many years did I serve in the Army? 1-20');
+      if (questionSix == 7) {
+        alert('Good Job')
+        questionRight++
+        break;
+      } else if (questionSix < 7) {
+        alert('That was too low')
+      } else if (questionSix > 7) {
+        alert('That was too high')
+      } guessCount++
+    } while (guessCount < 4);
+  }
+
+
+  function Question7() {
+    do {
+      var favMovies = ['harry potter', 'good will hunting', 'grandma\'s boy', 'drive', 'step brothers', 'forrest gump', 'any monty python', 'tropic thunder', 'saving private ryan', 'a beautiful mind'];
+      var favMoviesQuestion = prompt('What do you think my favorite movies are?');
+      if (favMovies[0] === favMoviesQuestion || favMovies[1] === favMoviesQuestion) {
+        alert('Good Job!')
+        questionRight++
+        break;
+      } else (!favMoviesQuestion); {
+        alert('Please try again')
+      }
+      guessCount++
+    } while (guessCount < 6);
+    alert('Here is a list of my favorite movies ' + favMovies);
+  }
+
+  Question1();
+  Question2();
+  Question3();
+  Question4();
+  Question5();
+  Question6();
+  Question7();
+
+
+  alert('Congratulations ' + yourName + ', you got ' + questionRight + ' out of ' + numQuestions);
 }
-
-Question1();
-Question2();
-Question3();
-Question4();
-Question5();
-Question6();
-Question7();
-
-
-alert('Congratulations ' + yourName + ', you got ' + questionRight + ' out of ' + numQuestions);
-// // };
-
